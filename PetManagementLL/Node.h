9fbfe859ceb2cb78b1ed117a6ecb06295f3ca7e1
Nhut Ngo <nhut.ngo@opswat.com>
2021-06-07 20:04:14 +0700

    Fix build failure

--- PetManagementLL/Node.h
+++ PetManagementLL/Node.h
@@ -3,20 +3,73 @@
 #include "Cat.h"
 #include "Dog.h"
 
-template <class T> 
+template <class T>
 class Node
 {
 protected:
 	T data_;
-	Node* next_;
+	Node<T>* next_;
 public:
 	Node();
 	~Node();
 	T getData();
 	void setData(T data);
-	Node* getNext();
-	void setNext(Node* next);
-	static Node* getNewNode(std::istream& is);
+	Node<T>* getNext();
+	void setNext(Node<T>* next);
+	static Node<T>* getNewNode(std::istream& is)
+	{
+		Node<T>* newNode = new Node<T>();
+		if (newNode == NULL) {
+			std::cout << "\nCan't create new node!\n";
+			return NULL;
+		}
+		else {
+			char line[10] = {};
+			is.getline(line, 100);
+			if (line[0] == '1') {
+				T newPet = new Dog();
+				newPet->enterInfo(is);
+				newNode->setData(newPet);
+			}
+			else if (line[0] == '2')
+			{
+				T newPet = new Cat();
+				newPet->enterInfo(is);
+				newNode->setData(newPet);
+			}
+		}
+		return newNode;
+	}
 
 };
 
+
+template <class T>
+Node<T>::Node() {
+	next_ = nullptr;
+}
+
+template <class T>
+Node<T>::~Node() {
+	next_ = NULL;
+}
+
+template <class T>
+T Node<T>::getData() {
+	return data_;
+}
+
+template <class T>
+void Node<T>::setData(T data) {
+	this->data_ = data;
+}
+
+template <class T>
+Node<T>* Node<T>::getNext() {
+	return next_;
+}
+
+template <class T>
+void Node<T>::setNext(Node<T>* next) {
+	this->next_ = next;
+}
