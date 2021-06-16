884eb5d7c1c9a8869aa731491fdc9f61fc5468b3
Linh Le <hoanglinh.1299@gmail.com>
2021-06-16 12:31:50 +0700

    add a bitmap, draw and paint a square

--- DesktopApp/Source.cpp
+++ DesktopApp/Source.cpp
@@ -5,7 +5,8 @@
 #include <windows.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
+HBITMAP hBitmap = NULL;
+HINSTANCE hInst = NULL;
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
     // Register the window class.
@@ -23,7 +24,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     HWND hwnd = CreateWindowEx(
         0,                              // Optional window styles.
         CLASS_NAME,                     // Window class
-        L"Learn to Program Windows",    // Window text
+        L"Mario Game",    // Window text
         WS_OVERLAPPEDWINDOW,            // Window style
 
         // Size and position
@@ -61,13 +62,41 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         PostQuitMessage(0);
         return 0;
 
+    case WM_CREATE:
+        hBitmap = (HBITMAP)LoadImage(hInst, L"mario.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+        break;
+
     case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        EndPaint(hwnd, &ps);
-    }
+       
+        {
+            // Set up the device context for drawing.
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            BITMAP          bitmap;
+            HDC             hdcMem;
+            HGDIOBJ         oldBitmap;
+
+            HPEN hpenOld = static_cast<HPEN>(SelectObject(hdc, GetStockObject(DC_PEN)));
+            HBRUSH hbrushOld = static_cast<HBRUSH>(SelectObject(hdc, GetStockObject(NULL_BRUSH)));
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 20));
+            RECT rc1;
+            GetClientRect(hwnd, &rc1);
+            rc1.right = 94;
+            rc1.bottom = 102;
+            InflateRect(&rc1, -5, -5);
+            SetDCPenColor(hdc, RGB(255, 0, 0));    // red
+            Rectangle(hdc, rc1.left, rc1.top, rc1.right, rc1.bottom);
+            SelectObject(hdc, hpenOld);
+            SelectObject(hdc, hbrushOld);
+            //load bitmap
+            hdcMem = CreateCompatibleDC(hdc);
+            oldBitmap = SelectObject(hdcMem, hBitmap);
+            GetObject(hBitmap, sizeof(bitmap), &bitmap);
+            BitBlt(hdc, 13, 13, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCCOPY);
+            SelectObject(hdcMem, oldBitmap);
+            DeleteDC(hdcMem);
+            EndPaint(hwnd, &ps);
+        }
     return 0;
 
 
