0a403818abe896a3662735c78bde83ea86b376b2
Linh Le <hoanglinh.1299@gmail.com>
2021-06-18 18:01:32 +0700

    add OnPaint function to paint the window when press arrow buttons

--- Mario/DesktopApp/Source.cpp
+++ Mario/DesktopApp/Source.cpp
@@ -3,6 +3,15 @@
 #endif 
 
 #include <windows.h>
+#include <WindowsX.h>
+BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct);
+void OnPaint(HWND hwnd);
+void OnClose(HWND hwnd);
+void OnDestroy(HWND hwnd);
+void clear_screen(HWND hwnd);
+
+int x = 300;
+int y = 400;
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
@@ -22,8 +31,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
-        900,
-        550,
+        1000,
+        600,
         NULL,
         NULL,
         hInstance,
@@ -54,80 +63,116 @@ HBITMAP hBitmap = NULL;
 HINSTANCE hInst = NULL;
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    PAINTSTRUCT ps;
-    HDC hdc;
-    BITMAP          bitmap;
-    HDC             hdcMem;
-    HGDIOBJ         oldBitmap;
-    RECT rc1;
     switch (uMsg)
     {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
+        HANDLE_MSG(hwnd, WM_CREATE, OnCreate);
+        HANDLE_MSG(hwnd, WM_PAINT, OnPaint);
+        HANDLE_MSG(hwnd, WM_CLOSE, OnClose);
 
-    case WM_CREATE:
-        hBitmap = (HBITMAP)LoadImage(hInst, L"mario.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-        break;
     case WM_KEYDOWN:
         switch (wParam)
         {
+        case VK_ESCAPE: 
+        {
+            DestroyWindow(hwnd);
+        } break;
         case VK_LEFT:
-
-            break;
+        {
+            x -= 10;
+            OnPaint(hwnd);
+        } break;
 
         case VK_RIGHT:
-
-            break;
-
+        {
+            x += 10;
+            OnPaint(hwnd);
+        } break;
         case VK_UP:
-
-            break;
+        {
+            y -= 10;
+            OnPaint(hwnd);
+        } break;
 
         case VK_DOWN:
-
-            break;
-        }
-        case WM_PAINT:
-
         {
-            hdc = BeginPaint(hwnd, &ps);
-            HPEN hpenOld = static_cast<HPEN>(SelectObject(hdc, GetStockObject(DC_PEN)));
-            HBRUSH hbrushOld = CreateSolidBrush(RGB(50, 150, 50));
-            FillRect(hdc, &ps.rcPaint, hbrushOld);
-            GetClientRect(hwnd, &rc1);
-            rc1.left = 100;
-            rc1.right = 175;
-            rc1.top = 10;
-            rc1.bottom = 85;
-            Rectangle(hdc, rc1.left, rc1.top, rc1.right, rc1.bottom);
-            SelectObject(hdc, hpenOld);
-            SelectObject(hdc, hbrushOld);
-            hbrushOld = CreateSolidBrush(RGB(255, 155, 50));
-            FillRect(hdc, &rc1, hbrushOld);
-
-            //load bitmap
-            hdcMem = CreateCompatibleDC(hdc);
-            oldBitmap = SelectObject(hdcMem, hBitmap);
-            GetObject(hBitmap, sizeof(bitmap), &bitmap);
-            BitBlt(hdc, 10, 10, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCCOPY);
-            SelectObject(hdcMem, oldBitmap);
-            DeleteDC(hdcMem);
-
-            EndPaint(hwnd, &ps);
+            y += 10;
+            OnPaint(hwnd);
+        } break;
         }
-        return 0;
 
 
-        case WM_CLOSE:
-            if (MessageBox(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
-            {
-                DestroyWindow(hwnd);
-            }
-            return 0;
         default:
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
         }
 
         return 0;
+}
+
+BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
+{
+    hBitmap = (HBITMAP)LoadImage(hInst, L"mario.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    return TRUE;
+}
+
+void OnPaint(HWND hwnd)
+{
+
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hwnd, &ps);
+    BITMAP          bitmap;
+    HDC             hdcMem;
+    HGDIOBJ         oldBitmap;
+    RECT rc1, rc2;
+    HPEN hpenOld = static_cast<HPEN>(SelectObject(hdc, GetStockObject(DC_PEN)));
+    HBRUSH hbrushOld = CreateSolidBrush(RGB(50, 150, 50));
+  
+    FillRect(hdc, &ps.rcPaint, hbrushOld);
+    GetWindowRect(hwnd, &rc1);
+    GetClientRect(hwnd, &rc1);
+    
+    rc1.left = 0;
+    rc1.right = 1900;
+    rc1.top = 470;
+    rc1.bottom = 475;
+
+    rc2.left = 200;
+    rc2.right = 205;
+    rc2.top = 400;
+    rc2.bottom = 475;
+
+    
+
+    Rectangle(hdc, rc1.left, rc1.top, rc1.right, rc1.bottom);
+    Rectangle(hdc, 200, 400, 205, 475);
+    Ellipse(hdc, 500, 300, 600, 350);
+    SelectObject(hdc, hpenOld);
+    SelectObject(hdc, hbrushOld);
+    hbrushOld = CreateSolidBrush(RGB(255, 155, 50));
+    FillRect(hdc, &rc1, hbrushOld);
+    hbrushOld = CreateSolidBrush(RGB(250, 55, 50));
+    FillRect(hdc, &rc2, hbrushOld);
+    //load bitmap
+    //length = 67
+    //hight = 75
+    hdcMem = CreateCompatibleDC(hdc);
+    oldBitmap = SelectObject(hdcMem, hBitmap);
+    GetObject(hBitmap, sizeof(bitmap), &bitmap);
+    BitBlt(hdc, x, y, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCCOPY);
+    SelectObject(hdcMem, oldBitmap);
+    DeleteDC(hdcMem);
+
+    EndPaint(hwnd, &ps);
+}
+
+void OnClose(HWND hwnd)
+{
+    if (MessageBox(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
+    {
+        DestroyWindow(hwnd);
+    }
+}
+
+void OnDestroy(HWND hwnd)
+{
+    PostQuitMessage(0);
 }
