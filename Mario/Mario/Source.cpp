62c3204e21ae098708c4e368066147d8b60b4673
Linh Le <hoanglinh.1299@gmail.com>
2021-06-23 18:17:31 +0700

    Create animated movement for main character

--- Mario/Mario/Source.cpp
+++ Mario/Mario/Source.cpp
@@ -4,15 +4,20 @@
 #include <windows.h>
 #include <WindowsX.h>
 
+#define WINDOW_WIDTH 1000;
+#define WINDOW_HEIGHT 600;
 
 void OnCreate(HWND hwnd);
 void OnPaint(HWND hwnd);
 void OnKeyDown(HWND hwnd, WPARAM wParam);
+void OnKeyUp(HWND hwnd, WPARAM wParam);
 void OnClose(HWND hwnd);
 
 
-int x = 300;
-int y = 400;
+int posX = 0;
+int posY = 400;
+int characterX = 0;
+int characterY = 0;
 int globalRunning = 1;
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -32,8 +37,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 		L"Mario Game",
 		//Disable window resizing
 		WS_OVERLAPPEDWINDOW & ~WS_THICKFRAME & ~WS_MAXIMIZEBOX,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
+		0,
+		0,
 		1000,
 		600,
 		NULL,
@@ -86,6 +91,9 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_KEYDOWN:
 		OnKeyDown(hwnd, wParam);
 		return 0;
+	case WM_KEYUP:
+		OnKeyUp(hwnd, wParam);
+		return 0;
 	case WM_CREATE:
 		OnCreate(hwnd);
 		return 0;
@@ -108,16 +116,68 @@ void OnKeyDown(HWND hwnd, WPARAM wParam)
 		OnClose(hwnd);
 		break;
 	case VK_LEFT:
-		x -= 10;
+		posX -= 10;
+		if (posX <= 0)
+		{
+			posX += 10;
+		}
+		characterY = 1;
+		if (characterX >=3)
+		{
+			characterX = 0;
+		}
+		else
+		{
+			characterX++;
+		}
 		break;
 	case VK_RIGHT:
-		x += 10;
+		posX += 10;
+		if (posX >= (1000 - 90))
+		{
+			posX -= 10;
+		}
+		characterY = 0;
+		if (characterX >= 3)
+		{
+			characterX = 0;
+		}
+		else
+		{
+			characterX++;
+		}
 		break;
 	case VK_UP:
-		y -= 10;
+		posY -= 40;
+		if (posY <= 350)
+		{
+			posY += 40;
+		}
+		characterX = 4;
 		break;
 	case VK_DOWN:
-		y += 10;
+		characterX = 5;
+		break;
+	}
+	InvalidateRect(hwnd, NULL, FALSE);
+}
+
+void OnKeyUp(HWND hwnd, WPARAM wParam)
+{
+	switch (wParam)
+	{
+	case VK_LEFT:
+		characterX = 0;
+		break;
+	case VK_RIGHT:
+		characterX = 0;
+		break;
+	case VK_UP:
+		posY += 40;
+		characterX = 0;
+		break;
+	case VK_DOWN:
+		characterX = 0;
 		break;
 	}
 	InvalidateRect(hwnd, NULL, FALSE);
@@ -132,7 +192,7 @@ void OnPaint(HWND hwnd)
 	HGDIOBJ         oldBitmap;
 	RECT rc1, rc2;
 	HPEN hpenOld = static_cast<HPEN>(SelectObject(hdc, GetStockObject(DC_PEN)));
-	HBRUSH hbrushOld = CreateSolidBrush(RGB(50, 150, 50));
+	HBRUSH hbrushOld = CreateSolidBrush(RGB(20, 95, 155));
 
 	FillRect(hdc, &ps.rcPaint, hbrushOld);
 	GetWindowRect(hwnd, &rc1);
@@ -158,12 +218,13 @@ void OnPaint(HWND hwnd)
 	hbrushOld = CreateSolidBrush(RGB(250, 55, 50));
 	FillRect(hdc, &rc2, hbrushOld);
 	//load bitmap
-	//length = 67
-	//hight = 75
+	//width = 90
+	//hight = 70
 	hdcMem = CreateCompatibleDC(hdc);
 	oldBitmap = SelectObject(hdcMem, hBitmap);
 	GetObject(hBitmap, sizeof(bitmap), &bitmap);
-	BitBlt(hdc, x, y, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCCOPY);
+	//BitBlt(hdc, x, y, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCCOPY);
+	BitBlt(hdc, posX, posY, 90, 70, hdcMem, 90*characterX, 70* characterY, SRCCOPY);
 	SelectObject(hdcMem, oldBitmap);
 	DeleteDC(hdcMem);
 
